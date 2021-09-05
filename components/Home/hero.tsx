34336ec0184c20800e335379374ad4c6ad5f6ac4
Abdulhamid <abdulhamidoumer3@gmail.com>
2021-09-06 01:22:40 +0300

    feat: better hero layout

--- components/Home/hero.tsx
+++ components/Home/hero.tsx
@@ -3,15 +3,18 @@ import myImage from '../../assets/images/abdulhamid-black-and-white.png';
 
 const Hero = () => {
   return (
-    <div className="flex justify-end items-center  bg-white h-full flex-col">
-      <h3 className="text-3xl w-3/4 text-center font-medium text-gray-600">
-        Hi, I Am{' '}
-        <span className="font-bold text-secondary-500">Abdulhamid Oumer</span>.{' '}
-        A Full Stack Software Engineer Located In Addis Ababa, Ethiopia.
-      </h3>
+    <>
+      <div className="flex pt-24 items-center  bg-white min-h-full flex-col">
+        <h3 className="text-4xl w-3/4 text-center font-medium text-gray-600">
+          Hi, I Am{' '}
+          <span className="font-bold text-secondary-500">Abdulhamid Oumer</span>
+          . A Full Stack Software Engineer Located In Addis Ababa, Ethiopia.
+        </h3>
 
-      <Image src={myImage} alt="Abdulhamid Oumer" />
-    </div>
+        <Image src={myImage} alt="Abdulhamid Oumer" />
+        <div className="w-full h-60 bg-primary-500" />
+      </div>
+    </>
   );
 };
 
